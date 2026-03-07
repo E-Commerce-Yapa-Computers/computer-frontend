1dc39ea7b0284ee07ac627afa7157db3475ea6cf
Geethx <jeewangeethanga@gmail.com>
2026-03-07 08:49:49 +0530

    overview page

--- src/pages/overview.jsx
+++ src/pages/overview.jsx
@@ -1,10 +1,25 @@
+import axios from "axios";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
 import { useParams } from "react-router-dom"
 
 export default function Overview() {
 
     const params = useParams();
 
-    
+    useEffect(() => {
+        axios.get(import.meta.env.VITE_API_URL + "/products/" + params.productId).then(
+            (response) => {
+                console.log(response.data)
+            }
+        ).catch(
+            (error) => {
+                console.log(error)
+                toast.error("Failed to fetch product details!")
+            }
+        )
+    }, []);
+
 
     return (
         <div className="w-full flex items-center justify-center min-h-screen">
